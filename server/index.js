12157502ef59b3a64354df35d9c0dcfe2469b983
Ubuntu <ubuntu@ip-172-31-50-235.ec2.internal>
2021-04-23 15:00:28 +0000

    clean up

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('Aaron Hall, King of Mongolia/Brooklyn')
-})
-app.listen(3000, () => console.log('Server running on port 3000'))
